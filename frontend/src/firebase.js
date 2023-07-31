1e12e6c4e2e2d55c77f83faace25f43eefc6afd8
Logan Williams <logan@bellingcat.com>
2023-07-31 12:34:26 +0200

    Fix typo in Firebase config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_FIREBASE_APP_ID,
 };
 
